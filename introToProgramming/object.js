34e12acc1e41c1e2cd992dc5a8f53099de606ce6
AmucheOgbonna <amuche247cynthia@gmail.com>
2021-09-13 15:13:15 +0100

    Objects

--- introToProgramming/object.js
+++ introToProgramming/object.js
@@ -15,3 +15,25 @@ person.lastname = "Ogbonna";
 person.getFullname = function () {
     return this.firstname + " " + " " + this.lastname;
 };
+
+console.log(person);
+
+function changeFirstname(obj) {
+    obj.firstname = "steve";
+}
+changeFirstname(person);
+
+console.log(person);
+
+//Nested Objects
+var person = {
+    firstname: "Amuche",
+    "age": 30,
+    nationality: {
+        id: 1,
+        countryCode: "NG",
+        state: "Umuhia",
+        address: "Government college",
+    },
+    lastname: "Ogbonna",
+};
